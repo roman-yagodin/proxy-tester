c23f8518579ba20565a949140d6784ec03422135
roman-yagodin <roman.yagodin@gmail.com>
2023-10-03 17:41:17 +0300

    Second roll: - remove proxy_users.list (specify proxy username and password in proxies.list) - basic suppor for "#" comments in .list files - better output formatting

--- Program.fs
+++ Program.fs
@@ -1,11 +1,12 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
+open System
 open System.IO
 open System.Net
 open System.Net.Http
 
 let getCredentials(userName: string, password: string) =
-    if userName <> "default" then
+    if not (userName |> String.IsNullOrEmpty) then
         new NetworkCredential(userName, password)
     else
         CredentialCache.DefaultNetworkCredentials
@@ -24,28 +25,42 @@ let testConnection(proxy: string, proxyUserName: string, proxyPassword: string, 
     (
         try
             let value = client.GetAsync(host) |> Async.AwaitTask |> Async.RunSynchronously
-            value.StatusCode
+            value.StatusCode.ToString()
         with
-            | ex -> HttpStatusCode.InternalServerError
+        | ex -> ex.Message
     )
 
 let proxies = File.ReadAllLines "proxies.list"
 
-let proxyUsers= File.ReadAllLines "proxy-users.list"
-
 let hosts = File.ReadAllLines "hosts.list"
+
+let parseProxy (proxy) =
+    if proxy = "noproxy" then
+        ("noproxy", String.Empty, String.Empty)
+    else
+        let proxyUri = new Uri(proxy)
+        let proxyUserParts = proxyUri.UserInfo.Split(":")
+        let proxyUserName = proxyUserParts[0]
+        let proxyPassword = if proxyUserParts.Length > 1 then proxyUserParts[1] else ""
+        let proxy2 =
+            if proxyUri.Port > 0 then
+                String.Format("{0}://{1}:{2}", proxyUri.Scheme, proxyUri.Host, proxyUri.Port)
+            else
+                String.Format("{0}://{1}", proxyUri.Scheme, proxyUri.Host)
+        (proxy2, proxyUserName, proxyPassword)
+
+let formatUserName (userName, password) =
+    if (userName |> String.IsNullOrEmpty) then
+        "current user"
+    else
+        $"{userName}:{password}"
 
 for proxy in proxies do
     for host in hosts do
-        for proxyUser in proxyUsers do
-            let proxyUserParts = proxyUser.Split(":")
-            let proxyUserName = proxyUserParts[0]
-            let proxyPassword = if proxyUserParts.Length > 1 then proxyUserParts[1] else ""
-            let testResult = testConnection(proxy, proxyUserName, proxyPassword, host)
-            if testResult = HttpStatusCode.OK then
-                printfn "passed: %s via %s, %s:%s" host proxy proxyUserName proxyPassword 
-            else
-                printfn "not passed: %s via %s, %s:%s" host proxy proxyUserName proxyPassword 
+        if not (proxy.StartsWith("#")) && not (host.StartsWith("#")) then
+            let (proxy2, proxyUserName, proxyPassword) = parseProxy(proxy)
+            let testResult = testConnection( proxy2, proxyUserName, proxyPassword, host)
+            printf "%s via %s, %s - " host proxy2 (formatUserName(proxyUserName, proxyPassword))
+            printfn "%s" testResult
 
-
-
+printfn "Done."
